258609e9296903d4415b779fbc7cb819ae02621c
wenghao <632561223@qq.com>
2022-06-15 23:02:01 +0800

    tz

--- src/main/java/Strategy/TestStrategy.java
+++ src/main/java/Strategy/TestStrategy.java
@@ -17,15 +17,12 @@ public class TestStrategy {
         strategyCompute(new DivStrategy(), a, b);
         // 当然也能使用lambda
         strategyCompute((c, d) -> System.out.println("我是lambda method：" + (c + d)), a, b);
-        IStrategy l = (c, d) -> System.out.println("lambda new：" + (c-d));
+        IStrategy l = (c, d) -> System.out.println("lambda new：" + (c - d));
         l.compute(1, 100);
     }
 
     public void strategyCompute(IStrategy strategy, int a, int b) {
         strategy.compute(a, b);
-    }
-    public Integer t(Supplier<Integer> strategy) {
-        return strategy.get();
     }
 
 }
